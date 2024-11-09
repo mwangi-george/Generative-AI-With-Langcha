f3499af26a1755321d88d4b1d4881007aa37d649
George Mwangi <mwangigeorge648@gmail.com>
2024-11-09 11:32:02 +0300

    variables

--- 1-Basics/variables.py
+++ 1-Basics/variables.py
@@ -8,3 +8,19 @@ age = 23.4
 print(age, type(age))
 age = int(age)
 print(age, type(age))
+
+
+name: str = "George"
+print(name, type(name))
+
+name = 3
+print(name, type(name))
+
+name = bool(name)
+print(name, type(name))
+
+result: str = "Hello" + str(343)
+print(result)
+
+result = f"Hello {345}"
+print(result)
