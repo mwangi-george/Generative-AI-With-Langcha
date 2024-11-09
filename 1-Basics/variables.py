64c5a8aadbdde14708bab4a1ebce0e042702b64f
George Mwangi <mwangigeorge648@gmail.com>
2024-11-09 11:20:22 +0300

    variables

---
+++ 1-Basics/variables.py
@@ -0,0 +1,10 @@
+age = 23
+print(age, type(age))
+age = str(age)
+print(age, type(age))
+age = int(age)
+print(age, type(age))
+age = 23.4
+print(age, type(age))
+age = int(age)
+print(age, type(age))
